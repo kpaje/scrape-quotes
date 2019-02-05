42b0a0063e02163b280ec978e086f2ea8241e4d0
kpaje <kelvinmembership@gmail.com>
2019-02-05 13:31:19 -0800

    fixing mongo

--- server.js
+++ server.js
@@ -4,21 +4,25 @@ var mongoose = require("mongoose");
 var PORT = 3000;
 var app = express();
 
-// Configure middleware
-app.use(logger("dev"));
-app.use(express.urlencoded({
-  extended: true
-}));
+// Configure body parsing for AJAX requests
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("public"));
+// Serve up static assets
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
 
 //Routes
 require("./controllers/routes")(app);
 
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/scrape-quotes", {
-  useNewUrlParser: true
-});
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/scrape-quotes",
+  {
+    useCreateIndex: true,
+    useNewUrlParser: true
+  }
+);
 
 // Start the server
 app.listen(PORT, function () {
