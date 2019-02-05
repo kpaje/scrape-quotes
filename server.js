5a731d06e3d36d3c2104cbee38a6bb6e58560903
kpaje <kelvinmembership@gmail.com>
2019-02-05 15:27:31 -0800

    herokue

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ var mongoose = require("mongoose");
 var PORT = process.env.PORT || 3000;
 var app = express();
 var logger = require("morgan");
+var axios = require("axios");
+var cheerio = require("cheerio");
 const db = require("./models");
 
 // Parse request body as JSON
@@ -10,12 +12,51 @@ app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
-require("./controllers/routes")(app)
+// require("./controllers/routes")(app)
 
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/scrape-quotes";
 
 // Connect to the Mongo DB
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+mongoose.connect(MONGODB_URI);
+
+// Routes
+
+app.get("/scrape", function (req, res) {
+  axios.get("https://patrickmn.com/life/call-of-duty-war-quotes/").then(function (response) {
+      var $ = cheerio.load(response.data);
+      $("blockquote").each(function (i, element) {
+          var result = {};
+          result.title = $(this)
+              .text();
+          result.link = $(this)
+              .children("p")
+              .children("em")
+              .text();
+
+          db.Article.create(result)
+              .then(function (dbArticle) {
+                  console.log(dbArticle);
+              })
+              .catch(function (err) {
+                  console.log(err);
+              });
+      });
+
+      res.send("Scrape Complete");
+  });
+});
+
+// Route for getting all Articles from the db
+app.get("/articles", function (req, res) {
+  db.Article.find({})
+      .then(function (dbArticle) {
+          res.json(dbArticle);
+      })
+      .catch(function (err) {
+          res.json(err);
+      });
+});
+
 
 // Listen on the port
 app.listen(PORT, function () {
