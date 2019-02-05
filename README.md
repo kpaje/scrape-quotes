45a3707110c1e76e5413abd104785d5d85a23952
kpaje <kelvinmembership@gmail.com>
2019-02-05 13:18:46 -0800

    quotes working

---
+++ README.md
@@ -0,0 +1,18 @@
+War is Hell
+=====================================
+
+A hauntingly vivid depiction of quotes from history's greatest figures.
+
+The image of Charlie: The Chihuahua's longing glare, accurately characterizes the grim outlook of a war torn veteran.
+
+
+## Tools
+- Axios
+- Cheerios
+- Express
+- Mongoose
+- Node
+- Heroku
+
+
+
--- controllers/routes.js
+++ controllers/routes.js
@@ -39,42 +39,4 @@ module.exports = function (app) {
                 res.json(err);
             });
     });
-
-    // Route for grabbing a specific Article by id, populate it with it's note
-    app.get("/articles/:id", function (req, res) {
-        db.Article.findOne({
-                _id: req.params.id
-            })
-            .populate("note")
-            .then(function (dbArticle) {
-                res.json(dbArticle);
-            })
-            .catch(function (err) {
-                res.json(err);
-            });
-    });
-
-    // Route for saving/updating an Article's associated Note
-    app.post("/articles/:id", function (req, res) {
-        // Create a new note and pass the req.body to the entry
-        db.Note.create(req.body)
-            .then(function (dbNote) {
-                // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
-                // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
-                // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
-                return db.Article.findOneAndUpdate({
-                    _id: req.params.id
-                }, {
-                    note: dbNote._id
-                }, {
-                    new: true
-                });
-            })
-            .then(function (dbArticle) {
-                res.json(dbArticle);
-            })
-            .catch(function (err) {
-                res.json(err);
-            });
-    });
 };
